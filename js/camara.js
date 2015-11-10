a16d58e2a6ead9db1e7c95aaf7dc54340d40fb48
Amparo Megias Castillo <amparo@MacBook-Pro-de-Amparo.local>
2015-11-10 23:04:14 +0100

    corrección errores

--- js/camara.js
+++ js/camara.js
@@ -1,25 +1,22 @@
 $("document").ready(function() {
 	//Variable que va a almacenar los datos de la imagen
-	var miDataURL,
+	var miDataURL;
+	var miContexto;
 	
 	$titulocamara = $('#titulocamara');
 	$descripcioncamara = $('#descripcioncamara');
-	//En la variable miVideo se almacena una referencia al elemento con id media
-	$miVideo = $('#media');
+	
+	console.log($titulocamara);
 		
 	//Al elemento con id media le asigno alto y ancho de la ventana
-	$miVideo.width = window.innerWidth;
-	$miVideo.height = window.innerHeight;
+	$('#media').width = window.innerWidth;
+	$('#media').height = window.innerHeight;
 		
 	//En la variable miFoto se almacena una referencia al elemento con id foto
 	$miFoto = $('#foto');
 		
-	//En la variable micanvas se almacena una referencia al elemento con id canvas
-	$miCanvas = $('#canvas');
-		
 	// Almaceno la referencia al elemento type button con id grabar del formulario y le registro un detector para el evento 'onclick'*/
-	var miBoton=$('#grabar');
-	miBoton.on('click',nuevoitem);
+	$('#grabarcamara').on('click',nuevoitem);
 		
 	//El evento popupafterclose se lanza cuando la ventana(en este caso de alta) se cierra.
 	$('#altacamaraModal').on('popupafterclose',resetformalta);
@@ -32,11 +29,11 @@ $("document").ready(function() {
 
 //Esta función se ejecuta cuando se cierra la ventana modal que contiene el formulario de altas
 function resetformalta(){
-/*
-*Limpio todos los campos del formulario de alta.
-*/ 
-$titulocamara.val("");
-$descripcioncamara.val("");
+	/*
+	*Limpio todos los campos del formulario de alta.
+	*/ 
+	$('#titulocamara').val("");
+	$('#descripcioncamara').val("");
 }
 	
 //Esta función se ejecuta si el usuario accede a que la app tenga acceso a la cámara web.
@@ -46,22 +43,30 @@ function exito(stream){
 		
 	//Usando el método createObjectURL() se obtiene la URL que representa el stream
 	//La URL se asigna al atributo src de elemento <video>
-	$miVideo.attr('src', URL.createObjectURL(stream));
-		
-	//El vídeo se reproduce
-	$miVideo.play();
+	$('#media').attr('src', URL.createObjectURL(stream));
+	
+	/*
+	El objeto jquery $('#media'), no tiene el método play, este método se encuentra en el nativo elemento dom dentro del objeto. Por eso se produce un 	error si se intenta ejecutar $('#media').play()
+	El vídeo se reproduce
+	*/
+	$('#media').get(0).play();	
 	
 	//Se añade un detector para el evento onclick en el elemento <video>, que ejecuta el código para tomar una instatánea, cuando se hace click en el elemento con id foto
 	//Se toma el fotograma de vídeo actual y se dibuja en el lienzo, tomando una instantánea
-	$miFoto.bind( "click", function( $miVideo ) {
-    //Se crea el contexto del lienzo
-	$miContexto = $miCanvas.getContext('2d');
+	$miFoto.bind( "click", function() {
+		var miVideo = document.getElementById('media');
+		var miCanvas = document.getElementById('canvas');
 		
-	//Se llama al método de canvas drawImage() con una referencia al vídeo a los valores correspondientes al tamaño del elemento <canvas>
-	$miContexto.drawImage(this, 0, 0, 320, 240);
+		//Se crea el contexto del lienzo
+		miContexto = miCanvas.getContext('2d');
+		
+		//Se llama al método de canvas drawImage() con una referencia al vídeo a los valores correspondientes al tamaño del elemento <canvas>
+		miContexto.drawImage(miVideo, 0, 0, 320, 240);
 
-	//Método que devuelve los datos de la imagen
-	miDataURL = $miCanvas.toDataURL();			
+		//Método que devuelve los datos de la imagen
+		miDataURL = miCanvas.toDataURL();	
+		
+		$("#altacamaraModal").popup("open");		
    });	
 }
 	
@@ -70,11 +75,9 @@ function mostraerror(e){
 }
 	
 function nuevoitem(){
-		
-	//Almaceno las referencias al titulo y la descripción del formulario de alta
-	var titulo=$titulocamara.val();
-	var descripcion=$descripcioncamara.val();
-		
+	
+	console.log("estoy en nuevo item");
+			
 	/*
 	* El método getTime() me devuelde el número de mm desde 1970/01/01
 	* Lo voy a utilizar, junto con el string "img_" para generar una clave para el elemento que voy a almacenar
@@ -83,10 +86,11 @@ function nuevoitem(){
 	var time = currentDate.getTime();
 	var key = "img_" + time;
 	var img_id= key;
+	
 	//Almaceno en  un objeto todos los datos del item a grabar
 	var img_datos = {
-		titulo: titulo,
-		descripcion: descripcion,
+		titulo: $('#titulocamara').val(),
+		descripcion: $('#descripcioncamara').val(),
 		imagen: miDataURL		
 	};
 		
@@ -100,7 +104,9 @@ function nuevoitem(){
 	*Limpio todos los campos del formulario de alta.
 	*/ 
 	resetformalta();
-	$miContexto.clearRect(0,0,$miCanvas.width, $miCanvas.height);
+	var miCanvas = document.getElementById('canvas');
+
+	miContexto.clearRect(0,0,miCanvas.width, miCanvas.height);
 		
 	//Cierro la ventana modal donde se encuentra el formulario
 	$("#altacamaraModal" ).popup( "close" );
